ec35c3f08e9a851cebcc31165b3695fd4d09d9cc
Otavioqwert <otavioml404@gmail.com>
2025-12-23 21:44:08 -0300

    Add ParaisoDoceSidebar widget to App - shows when package is active

--- App.tsx
+++ App.tsx
@@ -13,6 +13,7 @@ import FeverSetupModal from './components/shops/FeverSetupModal';
 import FeverReportModal from './components/shops/FeverReportModal';
 import TokenFlipOverlay from './components/shops/TokenFlipOverlay';
 import { ParaisoDoceGame } from './components/ParaisoDoceGame';
+import { ParaisoDoceSidebar } from './components/ParaisoDoceSidebar';
 
 // MEMOIZED COMPONENTS
 const MemoSlotMachine = React.memo(SlotMachine);
@@ -116,6 +117,14 @@ const App: React.FC = () => {
                 />
             )}
 
+            {/* Paraiso Doce Sidebar Widget */}
+            {game.paraisoState && (
+                <ParaisoDoceSidebar
+                    bars={game.paraisoState.bars}
+                    isActive={game.isParaisoDoceActive}
+                />
+            )}
+
 
             <CreditCardManager
                 creditCardLevel={game.creditCardLevel}
@@ -162,8 +171,7 @@ const App: React.FC = () => {
                 >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
-                      <path d="M10 3.5a1.5 1.5 0 011.456 1.842l-1.01 4.418a.5.5 0 00.91.4l1.554-1.50
-                          4a1.5 1.5 0 012.122 2.121l-4.243 4.242a1.5 1.5 0 01-2.121 0l-4.242-4.242a1.5 1.5 0 112.12-2.121l1.555 1.554a.5.5 0 00.91-.4L8.544 5.342A1.5 1.5 0 0110 3.5z" clipRule="evenodd" />
+                      <path d="M10 3.5a1.5 1.5 0 011.456 1.842l-1.01 4.418a.5.5 0 00.91.4l1.554-1.504a1.5 1.5 0 012.122 2.121l-4.243 4.242a1.5 1.5 0 01-2.121 0l-4.242-4.242a1.5 1.5 0 112.12-2.121l1.555 1.554a.5.5 0 00.91-.4L8.544 5.342A1.5 1.5 0 0110 3.5z" clipRule="evenodd" />
                     </svg>
                 </button>
             </div>
